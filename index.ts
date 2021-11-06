705fa1acbd57c7babd04a4d0742bb0f306a42430
Gabriel Chaves <gabrielmoreirachaves@outlook.com>
2021-11-06 11:55:34 -0300

    feat: dockerize app

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { setupConnection } from './src/db/config';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || '5000';
 
 setupConnection();
 
--- src/db/config.ts
+++ src/db/config.ts
@@ -1,18 +1,20 @@
 import { Student } from '../entities/Student';
-import { createConnection } from 'typeorm';
+import { createConnection, ConnectionOptions } from 'typeorm';
 
 export const setupConnection = async () => {
-  return createConnection({
+  const config: ConnectionOptions = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
     entities: [Student],
     synchronize: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  });
+  };
 
-  console.log('Connected to database');
+  const withSSL = { ssl: { rejectUnauthorized: false } };
+  const withoutSSL = { ssl: false };
+
+  if (process.env.NODE_ENV === 'production') {
+    return createConnection({ ...config, ...withSSL });
+  }
+
+  return createConnection({ ...config, ...withoutSSL });
 };
